5b1bb9f13f554f3bb4a49109207fc5ef36709607
Alexander Truong <alexander.m.truong@gmail.com>
2018-09-28 22:38:50 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -2,11 +2,11 @@ def my_each(collection)
   if block_given?
     i = 0
   while i <  collection.length 
-    puts collection[i]
+    collection[i]
     i += 1 
   end
 else
-  puts "Huh?"
+  "Huh?"
 end
   
 end
